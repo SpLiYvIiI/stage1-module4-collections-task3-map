7dace87af3bb58b76f7bd5075ae91e18d6ec07e5
SpLiYvIiI <32437231+SpLiYvIiI@users.noreply.github.com>
2022-08-27 21:20:47 +0400

    some message

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,18 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> res = new HashMap<>();
+        for (Integer i :
+                sourceMap.keySet()) {
+            if (res.containsKey(sourceMap.get(i)))
+                continue;
+            else
+                res.put(sourceMap.get(i), i);
+        }
+        return res;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,18 @@
 package com.epam.mjc.collections.map;
 
-import java.util.Map;
+import java.util.*;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> res = new HashMap<>();
+        List<String> list = Arrays.asList(sentence.split("\\s"));
+        for (String x :
+                list) {
+            if (x.isEmpty())
+                continue;
+            String toLower = x.toLowerCase(Locale.ENGLISH).replaceAll("[^a-zA-Z0-9]", "");
+            res.put(toLower, res.getOrDefault(toLower, 0) + 1);
+        }
+        return res;
     }
 }
